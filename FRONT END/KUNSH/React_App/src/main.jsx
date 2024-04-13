62adcf20d16113158250805f17132e4678963858
KT-Developer <kunshtaneja2004@gmail.com>
2024-04-13 17:53:42 +0530

    complaint page

--- FRONT END/KUNSH/React_App/src/main.jsx
+++ FRONT END/KUNSH/React_App/src/main.jsx
@@ -7,15 +7,15 @@ import ReactDOM from 'react-dom/client'
 // import App19 from './home page/App19'
 // import './home page/index19.css'
 
-// import App20 from './Complaint Page/App20'
-// import './Complaint Page/index20.css'
+import App20 from './Complaint Page/App20'
+import './Complaint Page/index20.css'
 
 // import App21 from './Thread Page/App21'
 
 //import App22 from './profile page/App22'
 
-import App23 from './Data Page for Admin/App23'
-import './Data Page for Admin/index23.css'
+// import App23 from './Data Page for Admin/App23'
+// import './Data Page for Admin/index23.css'
 
 //import App24 from './Edit profile page/App24'
 
@@ -23,6 +23,6 @@ import './Data Page for Admin/index23.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <App /> */}
-    <App23 />
+    <App20 />
   </React.StrictMode>,
 )
